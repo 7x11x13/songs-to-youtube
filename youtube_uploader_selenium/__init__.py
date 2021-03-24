fc2c2b516bab64b4a3ae71fe736340cb2758a873
7x11x13 <gibustf@gmail.com>
2021-03-24 06:45:06 -0400

    Fix imports

--- youtube_uploader_selenium/__init__.py
+++ youtube_uploader_selenium/__init__.py
@@ -2,7 +2,9 @@
 
     Based on https://github.com/linouk23/youtube_uploader_selenium"""
 
-from selenium_firefox.firefox import Firefox, By, Keys
+from selenium_firefox.firefox import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from typing import Optional
 import time
