b3ad9c8fb4e4f656319bde46ab91dd459a2ee6db
7x11x13 <gibustf@gmail.com>
2021-05-15 00:23:52 -0400

    Bump version

--- const.py
+++ const.py
@@ -17,7 +17,7 @@ class CustomDataRole(IntEnum):
 
 ORGANIZATION = "7x11x13"
 APPLICATION = "songs-to-youtube"
-VERSION = "v0.7"
+VERSION = "v0.7.1"
 SETTINGS_FILENAME = "v0.7settings"
 
 SUPPORTED_IMAGE_FILTER = "Images (*.bmp *.cur *.gif *.icns *.ico *.jpeg *.jpg *.pbm *.pgm *.png *.ppm *.svg *.svgz *.tga *.tif *.tiff *.wbmp *.webp *.xbm *.xpm)"
